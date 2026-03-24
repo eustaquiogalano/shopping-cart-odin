959ad14d4c09ab01df5dc6a2289a093b747bda6d
eustaquio <eustaquio.galano@gmail.com>
2026-03-24 13:20:09 +0800

    build: install @chakra-ui/react

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 import "./global.css";
 
 import routes from "./routes";
@@ -10,6 +12,8 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <ChakraProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </ChakraProvider>
   </StrictMode>
 );
