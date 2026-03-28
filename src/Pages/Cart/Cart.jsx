238a84d5173da2cd912c8fd3f2a70b1b81e7dca5
eustaquio <eustaquio.galano@gmail.com>
2026-03-28 10:56:32 +0800

    feat: render products in the cart

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -14,12 +14,11 @@ import {
   StatNumber,
 } from "@chakra-ui/react";
 
-import useDataFetcher from "../../hooks/useDataFetcher";
-
 import styles from "./Cart.module.css";
+import { useOutletContext } from "react-router";
 
 export default function Cart() {
-  const { laptops } = useDataFetcher();
+  const { laptops, error } = useOutletContext();
 
   return (
     <main className={styles.cartMain}>
@@ -27,54 +26,57 @@ export default function Cart() {
         {laptops &&
           laptops.map((laptop) => {
             return (
-              <Card
-                direction={{ base: "column", sm: "row" }}
-                overflow="hidden"
-                variant="outline"
-                display="flex"
-                flexShrink="0"
-                p="1rem"
-              >
-                <Image
-                  objectFit="cover"
-                  maxW={{ base: "100%", sm: "200px" }}
-                  src={laptop.images[1]}
-                  alt="Caffe Latte"
-                />
-
-                <Stack flex="1">
-                  <CardBody>
-                    <Heading size="md">{laptop.title}</Heading>
-                  </CardBody>
-
-                  <CardFooter display="flex" gap=".5rem">
-                    <Button variant="solid" colorScheme="brown">
-                      +
-                    </Button>
-                    <Input
-                      placeholder="0"
-                      textAlign="center"
-                      w="20%"
-                      type="number"
-                      colorScheme="brown"
-                    />
-                    <Button variant="solid" colorScheme="brown">
-                      -
-                    </Button>
-                  </CardFooter>
-                </Stack>
-
-                <Stat
-                  flex="0 0 auto"
+              laptop.addedToCart && (
+                <Card
+                  direction={{ base: "column", sm: "row" }}
+                  overflow="hidden"
+                  variant="outline"
                   display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  w="20%"
+                  flexShrink="0"
+                  p="1rem"
+                  key={laptop.id}
                 >
-                  <StatLabel fontSize="1rem">Price:</StatLabel>
-                  <StatNumber>£0.00</StatNumber>
-                </Stat>
-              </Card>
+                  <Image
+                    objectFit="cover"
+                    maxW={{ base: "100%", sm: "200px" }}
+                    src={laptop.images[1]}
+                    alt="Caffe Latte"
+                  />
+
+                  <Stack flex="1">
+                    <CardBody>
+                      <Heading size="md">{laptop.title}</Heading>
+                    </CardBody>
+
+                    <CardFooter display="flex" gap=".5rem">
+                      <Button variant="solid" colorScheme="brown">
+                        +
+                      </Button>
+                      <Input
+                        placeholder="0"
+                        textAlign="center"
+                        w="20%"
+                        type="number"
+                        colorScheme="brown"
+                      />
+                      <Button variant="solid" colorScheme="brown">
+                        -
+                      </Button>
+                    </CardFooter>
+                  </Stack>
+
+                  <Stat
+                    flex="0 0 auto"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    w="20%"
+                  >
+                    <StatLabel fontSize="1rem">Price:</StatLabel>
+                    <StatNumber>£0.00</StatNumber>
+                  </Stat>
+                </Card>
+              )
             );
           })}
       </div>
