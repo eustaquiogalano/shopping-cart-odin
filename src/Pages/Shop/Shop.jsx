973776944e184e605a9d9a0dbcbc4181df635dbc
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-04-18 22:21:18 +0800

    refactor: update Shop page layout and styling

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -27,30 +27,51 @@ export default function Shop() {
             return (
               <Card className="h-full overflow-hidden p-0">
                 <img
-                  src={laptop.images[0]}
+                  src={laptop.thumbnail}
                   alt=""
                   className="w-full h-[180px] object-cover"
                 />
+                <CardHeader>
+                  <CardTitle className="">{laptop.title}</CardTitle>
+                  <CardDescription className="">
+                    ${laptop.price}
+                  </CardDescription>
+                </CardHeader>
                 <CardContent className="flex flex-col p-4 gap-4 h-full">
-                  <div className="flex justify-between items-start">
-                    <div>
-                      <p className="font-medium">{laptop.title}</p>
+                  <CardDescription>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">Brand:</span>
+                      <span>{laptop.brand}</span>
                     </div>
-                    <p className="font-medium">${laptop.price}</p>
-                  </div>
-                  <div className="flex justify-between gap-1 mt-auto ">
-                    <Button variant="default" className="flex-1">
-                      Buy now
-                    </Button>
-                    <Button
-                      variant="outline"
-                      className="flex-1"
-                      onClick={() => addLaptopToCart(laptop.id, laptops)}
-                    >
-                      Add to cart
-                    </Button>
-                  </div>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">Dimensions:</span>
+                      <span>
+                        {laptop.dimensions.depth} x {laptop.dimensions.width} x{" "}
+                        {laptop.dimensions.height}
+                      </span>
+                    </div>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">Stock:</span>
+                      <span>{laptop.stock}</span>
+                    </div>
+                    <div className="flex justify-between">
+                      <span className="text-muted-foreground">Weight:</span>
+                      <span>{laptop.weight}</span>
+                    </div>
+                  </CardDescription>
                 </CardContent>
+                <CardFooter className="flex justify-between gap-1  ">
+                  <Button variant="default" className="flex-1">
+                    Buy now
+                  </Button>
+                  <Button
+                    variant="outline"
+                    className="flex-1"
+                    onClick={() => addLaptopToCart(laptop.id, laptops)}
+                  >
+                    Add to cart
+                  </Button>
+                </CardFooter>
               </Card>
             );
           })}
