4ec03ea2c966b4d3130380d7b045f73bc7e2e1d1
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-04-21 11:44:05 +0800

    refactor: update feature button styles and active state logic

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { House, Search, ShoppingBag, User } from "lucide-react";
-import { NavLink, Outlet } from "react-router";
+import { Link, NavLink, Outlet, useLocation } from "react-router";
 
 import styles from "./BottomNav.module.css";
 import { Button } from "../ui/button";
 
 function BottomNav() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const el = document.querySelector(hash);
+      if (el) el.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [hash]);
+
   return (
     <nav className="flex justify-around gap-[0.25] fixed bottom-0 left-0 right-0 h-14 bg-background border-t border-stone-300 z-10">
       <NavLink
         to="/"
+        end
         className={({ isActive }) =>
-          `${styles.navLinks} ${isActive ? styles.active : ""}`
+          `${styles.navLinks} ${isActive && hash === "" ? styles.active : ""}`
         }
       >
         <House strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
@@ -28,14 +38,17 @@ function BottomNav() {
         Search
       </NavLink>
 
-      <Button variant="outline" className={`${styles.navLinks} `}>
-        <a
-          href="#featured"
-          className="text-xs flex flex-col items-center justify-between h-full"
+      <Button
+        variant="outline"
+        className="border-0 h-full rounded-none p-0 w-[20%]"
+      >
+        <Link
+          to="/#featured"
+          className={`${hash === "#featured" ? styles.active : ""} p-[.5rem] font-[helvetica] text-[.75rem] flex flex-col items-center justify-between h-full w-full`}
         >
           <ShoppingBag strokeWidth={1.5} className="w-[1.20rem] flex-1 " />
           Featured
-        </a>
+        </Link>
       </Button>
 
       <NavLink
