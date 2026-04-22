cc44f0a22ac038874527bfd0b8e017e9c2f47857
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-04-22 14:21:32 +0800

    refactor: optimize Home page for laptop screens

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -27,88 +27,92 @@ export default function Home() {
 
   return (
     <main className={styles.homeMain}>
-      {/* Hero section */}
-      <section className="flex flex-col h-fit p-[1.5rem] gap-[.75rem] w-full relative border border-stone-300 rounded-xl overflow-hidden">
-        <Badge variant="secondary" className=" ">
-          New Arrivals
-        </Badge>
-        <div className="w-full h-55 overflow-hidden rounded-lg md:absolute md:right-[1.5rem] md:top-1/2 md:-translate-y-1/2 md:w-[40%] md:h-[70%]">
-          <img
-            src={heroImg}
-            alt="Laptop in a modern workspace"
-            className="w-full h-full object-cover"
-          />
-        </div>
-        <div className="md:w-[50%] ">
-          <h2 className="text-2xl">Tech that fits you everyday.</h2>
-          <p className="text-xs text-muted-foreground">
-            Curate eletronics. Simple prices. Fast shipping.
-          </p>
-        </div>
-        <div className="flex justify-between gap-[.75rem] md:w-[50%] ">
-          <Button variant="default" className="flex-1 md:h-[3rem]">
-            Shop Now!
-          </Button>
-          <Button variant="outline" className="flex-1 md:h-[3rem]">
-            View Deals
-          </Button>
-        </div>
-      </section>
+      <div className="max-w-4xl mx-auto flex flex-col gap-4">
+        {/* Hero section */}
+        <section className="flex flex-col h-fit p-[1.5rem] gap-[.75rem] lg:gap-[1.5rem] w-full relative border border-stone-300 rounded-xl overflow-hidden">
+          <Badge variant="secondary" className=" ">
+            New Arrivals
+          </Badge>
+          <div className="w-full h-55 overflow-hidden rounded-lg md:absolute md:right-[1.5rem] md:top-1/2 md:-translate-y-1/2 md:w-[40%] md:h-[70%]">
+            <img
+              src={heroImg}
+              alt="Laptop in a modern workspace"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="md:w-[50%] ">
+            <h2 className="text-2xl lg:text-4xl">
+              Tech that fits you everyday.
+            </h2>
+            <p className="text-xs lg:text-sm text-muted-foreground">
+              Curate eletronics. Simple prices. Fast shipping.
+            </p>
+          </div>
+          <div className="flex justify-between gap-[.75rem] md:w-[50%] ">
+            <Button variant="default" className="flex-1 md:h-[3rem]">
+              Shop Now!
+            </Button>
+            <Button variant="outline" className="flex-1 md:h-[3rem]">
+              View Deals
+            </Button>
+          </div>
+        </section>
 
-      {/* Category Section */}
-      <section className="flex flex-col h-fit gap-[.5rem] w- full">
-        <h3>Categories</h3>
-        <div className="flex gap-[.5rem] justify-between overflow-x-auto ">
-          {categories.map((category) => (
-            <Link
-              className="flex flex-col h-fit flex-1"
-              key={category.name}
-              to={category.link}
-            >
-              <Button
-                className="flex flex-col h-fit p-[1rem] text-xs "
-                variant="outline"
+        {/* Category Section */}
+        <section className="flex flex-col h-fit gap-[.5rem] w- full">
+          <h3>Categories</h3>
+          <div className="flex gap-[.5rem] justify-between overflow-x-auto ">
+            {categories.map((category) => (
+              <Link
+                className="flex flex-col h-fit flex-1"
+                key={category.name}
+                to={category.link}
               >
-                <category.iconName className="w-6 h-6" />
-                {category.name}
-              </Button>
-            </Link>
-          ))}
-        </div>
-      </section>
+                <Button
+                  className="flex flex-col h-fit p-[1rem] text-xs "
+                  variant="outline"
+                >
+                  <category.iconName className="w-6 h-6" />
+                  {category.name}
+                </Button>
+              </Link>
+            ))}
+          </div>
+        </section>
 
-      {/* Featured Section */}
-      <section
-        id="featured"
-        className="flex flex-col h-fit gap-[.5rem] w- full"
-      >
-        <h3>Featured</h3>
-        <div className="grid grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))] auto-rows-min gap-[1rem]  overflow-y-auto min-h-0 flex-1">
-          {laptops &&
-            laptops
-              .slice(0, 4)
-              .map((laptop) => (
-                <ProductCard
-                  key={laptop.id}
-                  image={laptop.thumbnail}
-                  badge="New"
-                  name={laptop.title}
-                  spec={laptop.description}
-                  price={`$${laptop.price.toFixed(2)}`}
-                />
-              ))}
-        </div>
-      </section>
+        {/* Featured Section */}
+        <section
+          id="featured"
+          className="flex flex-col h-fit gap-[.5rem] w- full"
+        >
+          <h3>Featured</h3>
+          <div className="grid grid-cols-[repeat(auto-fill,_minmax(150px,_1fr))] auto-rows-min gap-[1rem]  overflow-y-auto min-h-0 flex-1">
+            {laptops &&
+              laptops
+                .slice(0, 4)
+                .map((laptop) => (
+                  <ProductCard
+                    key={laptop.id}
+                    image={laptop.thumbnail}
+                    badge="New"
+                    name={laptop.title}
+                    spec={laptop.description}
+                    price={`$${laptop.price.toFixed(2)}`}
+                  />
+                ))}
+          </div>
+        </section>
 
-      {/* Limited Time Section */}
-      <section className="flex flex-col h-fit gap-[.5rem] w- full">
-        <PromoBanner
-          label="Limited time"
-          title="Up to 30% off accessories"
-          description="Cases, cables, chargers and more."
-          cta="Shop deals"
-        />
-      </section>
+        {/* Limited Time Section */}
+        <section className="flex flex-col h-fit gap-[.5rem] w- full">
+          <PromoBanner
+            label="Limited time"
+            title="Up to 30% off accessories"
+            description="Cases, cables, chargers and more."
+            cta="Shop deals"
+          />
+        </section>
+      </div>
     </main>
   );
 }
