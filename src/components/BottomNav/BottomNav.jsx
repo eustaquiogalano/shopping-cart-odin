5fc7e7da8fcf9807744778ca5c804b575ab88be4
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-04-23 13:01:23 +0800

    refactor: update BottomNav to appear floating

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -16,51 +16,56 @@ function BottomNav() {
   }, [hash]);
 
   return (
-    <nav className="flex justify-around gap-[0.25] fixed bottom-0 left-0 right-0 h-14 bg-background border-t border-stone-300 z-10 md:hidden">
-      <NavLink
-        to="/"
-        end
-        className={({ isActive }) =>
-          `${styles.navLinks} ${isActive && hash === "" ? styles.active : ""}`
-        }
+    <div className="fixed bottom-0 left-0 right-0 h-fit p-4 bg-transparent">
+      <nav
+        className="flex justify-around gap-[0.25]  bg-background 
+      border border-stone-300 z-10 rounded-xl md:hidden"
       >
-        <House strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
-        Home
-      </NavLink>
-
-      <NavLink
-        to="/shop"
-        className={({ isActive }) =>
-          `${styles.navLinks} ${isActive ? styles.active : ""}`
-        }
-      >
-        <Search strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
-        Search
-      </NavLink>
-
-      <Button
-        variant="outline"
-        className="border-0 h-full rounded-none p-0 w-[20%]"
-      >
-        <Link
-          to="/#featured"
-          className={`${hash === "#featured" ? styles.active : ""} p-[.5rem] font-[helvetica] text-[.75rem] flex flex-col items-center justify-between h-full w-full`}
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            `${styles.navLinks} ${isActive && hash === "" ? styles.active : ""}`
+          }
         >
-          <ShoppingBag strokeWidth={1.5} className="w-[1.20rem] flex-1 " />
-          Featured
-        </Link>
-      </Button>
+          <House strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
+          Home
+        </NavLink>
 
-      <NavLink
-        to="/profile"
-        className={({ isActive }) =>
-          `${styles.navLinks} ${isActive ? styles.active : ""}`
-        }
-      >
-        <User strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
-        Profile
-      </NavLink>
-    </nav>
+        <NavLink
+          to="/shop"
+          className={({ isActive }) =>
+            `${styles.navLinks} ${isActive ? styles.active : ""}`
+          }
+        >
+          <Search strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
+          Search
+        </NavLink>
+
+        <Button
+          variant="outline"
+          className="border-0 h-full rounded-none p-0 w-[20%]"
+        >
+          <Link
+            to="/#featured"
+            className={`${hash === "#featured" ? styles.active : ""} p-[.5rem] font-[helvetica] text-[.75rem] flex flex-col items-center justify-between h-full w-full`}
+          >
+            <ShoppingBag strokeWidth={1.5} className="w-[1.20rem] flex-1 " />
+            Featured
+          </Link>
+        </Button>
+
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            `${styles.navLinks} ${isActive ? styles.active : ""}`
+          }
+        >
+          <User strokeWidth={1.5} className="w-[1.20rem] h-[1.20rem]" />
+          Profile
+        </NavLink>
+      </nav>
+    </div>
   );
 }
 
