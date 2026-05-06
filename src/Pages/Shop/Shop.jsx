c323fd5f37b8fc2b48f1be3e5dfd24ace5d2d149
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-05-06 17:16:52 +0800

    refactor(ProductCard): extract to new component and improve layout

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,17 +1,7 @@
 import { useOutletContext } from "react-router";
 
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  CardContent,
-} from "@/components/ui/card";
-
-import { Button } from "@/components/ui/button";
-
 import styles from "./Shop.module.css";
+import ProductCard from "@/components/ProductCard/ProductCard";
 
 export default function Shop() {
   const { laptops, addLaptopToCart } = useOutletContext();
@@ -24,54 +14,7 @@ export default function Shop() {
         {laptops &&
           laptops.map((laptop) => {
             return (
-              <Card className="h-full overflow-hidden p-0">
-                <img
-                  src={laptop.thumbnail}
-                  alt=""
-                  className="w-full h-[180px] object-cover"
-                />
-                <CardHeader>
-                  <CardTitle className="">{laptop.title}</CardTitle>
-                  <CardDescription className="">
-                    ${laptop.price}
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="flex flex-col p-4 gap-4 h-full">
-                  <CardDescription>
-                    <div className="flex justify-between">
-                      <span className="text-muted-foreground">Brand:</span>
-                      <span>{laptop.brand}</span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-muted-foreground">Dimensions:</span>
-                      <span>
-                        {laptop.dimensions.depth} x {laptop.dimensions.width} x{" "}
-                        {laptop.dimensions.height}
-                      </span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-muted-foreground">Stock:</span>
-                      <span>{laptop.stock}</span>
-                    </div>
-                    <div className="flex justify-between">
-                      <span className="text-muted-foreground">Weight:</span>
-                      <span>{laptop.weight}</span>
-                    </div>
-                  </CardDescription>
-                </CardContent>
-                <CardFooter className="flex justify-between gap-1  ">
-                  <Button variant="default" className="flex-1">
-                    Buy now
-                  </Button>
-                  <Button
-                    variant="outline"
-                    className="flex-1"
-                    onClick={() => addLaptopToCart(laptop.id, laptops)}
-                  >
-                    Add to cart
-                  </Button>
-                </CardFooter>
-              </Card>
+              <ProductCard product={laptop} handleAddToCart={addLaptopToCart} />
             );
           })}
       </div>
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,222 @@
+import React from "react";
+import { Card, CardContent, CardFooter } from "../ui/card";
+import { Button } from "../ui/button";
+import { Badge } from "../ui/badge";
+
+function ProductCard(props) {
+  console.log(props);
+
+  return (
+    <Card>
+      <CardContent>
+        {/* image */}
+        <div className=" w-full">
+          <img src={props.product.images[1]} alt="product image" />
+        </div>
+
+        {/* badge */}
+        <div className="pt-6">
+          <div className="mb-4 flex items-center justify-between gap-4">
+            <Badge className="p-2"> Up to 35% off </Badge>
+
+            <div className="flex items-center justify-end gap-1">
+              <Button
+                variant="ghost"
+                type="button"
+                data-tooltip-target="tooltip-quick-look"
+                className="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                <span className="sr-only"> Quick look </span>
+                <svg
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </Button>
+              <div
+                id="tooltip-quick-look"
+                role="tooltip"
+                className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                data-popper-placement="top"
+              >
+                Quick look
+                <div className="tooltip-arrow" data-popper-arrow=""></div>
+              </div>
+              <Button
+                variant="ghost"
+                type="button"
+                data-tooltip-target="tooltip-add-to-favorites"
+                className="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                <span className="sr-only"> Add to Favorites </span>
+                <svg
+                  className="h-full w-full"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z"
+                  />
+                </svg>
+              </Button>
+              <div
+                id="tooltip-add-to-favorites"
+                role="tooltip"
+                className="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700"
+                data-popper-placement="top"
+              >
+                Add to favorites
+                <div className="tooltip-arrow" data-popper-arrow=""></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* product name */}
+        <a
+          href="#"
+          className="text-lg font-semibold leading-tight hover:underline"
+        >
+          Apple iMac 27", 1TB HDD, Retina 5K Display, M3 Max
+        </a>
+
+        {/* ratings */}
+        <div className="mt-2 flex items-center gap-2">
+          <div className="flex items-center">
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+            </svg>
+
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+            </svg>
+
+            <svg
+              className="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+            </svg>
+
+            <svg
+              class="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+            </svg>
+
+            <svg
+              class="h-4 w-4 text-yellow-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+            </svg>
+          </div>
+
+          <p className="text-sm font-medium">{props.product.rating}</p>
+          <p className="text-sm font-medium">{`(${props.product.reviews.length})`}</p>
+        </div>
+
+        {/* other info */}
+        <ul className="mt-2 flex items-center gap-4">
+          <li className="flex items-center gap-2">
+            <svg
+              className="h-4 w-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+              />
+            </svg>
+            <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+              Fast Delivery
+            </p>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <svg
+              className="h-4 w-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="M8 7V6c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1h-1M3 18v-7c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+              />
+            </svg>
+            <p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+              Best Price
+            </p>
+          </li>
+        </ul>
+      </CardContent>
+      <CardFooter className="flex items-center justify-around w-full h-fit g-4">
+        <p className="text-center text-2xl font-extrabold leading-tight ">
+          $1,699
+        </p>
+
+        <Button
+          onClick={() => props.handleAddToCart(props.product.id)}
+          className="py-5"
+        >
+          Add to cart
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default ProductCard;
