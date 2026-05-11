7e2853d9a864e4198bd4802dc7c6c7acb39e2ae8
eustaquiogalano <135113924+eustaquiogalano@users.noreply.github.com>
2026-05-11 15:21:43 +0800

    refactor(Home): update FeaturedCard components
    - Convert div to shadcn Card component

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -1,28 +1,35 @@
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 export default function FeaturedCard({ image, badge, name, spec, price }) {
   return (
-    <div className="bg-card text-secondary-foreground border  rounded-xl overflow-hidden">
-      <div className="h-28  bg-secondary flex items-center justify-center">
-        <img src={image} alt={name} className="w-16 h-16 object-contain" />
-      </div>
-
-      <div className="p-3 text-card-foreground">
+    <Card className="">
+      <CardContent className="flex-1">
+        {/* <div className="w-full bg-secondary flex items-center justify-center"> */}
+        <img src={image} alt={name} className="object-contain w-full" />
+        {/* </div> */}
         {badge && (
           <Badge className="mb-2 text-[10px] bg-accent text-accent-foreground">
             {badge}
           </Badge>
         )}
-        <p className="text-sm font-medium">{name}</p>
-        <p className="text-xs mb-3">OLED · 256GB</p>
-        <div className="flex justify-between items-center ">
-          <span className="text-sm font-medium">{price}</span>
-          <Button variant="default" size="sm" className="text-xs h-7">
-            Add
-          </Button>
-        </div>
-      </div>
-    </div>
+        <p className="text-lg font-medium mb-1">{name}</p>
+        <p className="text-xs">OLED · 256GB</p>
+      </CardContent>
+
+      <CardFooter className="flex justify-center items-center ">
+        <Button variant="default" size="sm" className="text-xs h-7 w-full">
+          See in Full
+        </Button>
+      </CardFooter>
+      {/* <div className="p-3 text-card-foreground"></div> */}
+    </Card>
   );
 }
